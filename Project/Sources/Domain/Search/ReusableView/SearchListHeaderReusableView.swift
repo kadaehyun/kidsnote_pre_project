e4b93fc7033846752553ade0c27a3aeb46a377bd
가대현 <daehyun.ka@musinsa.com>
2024-03-28 16:13:25 +0900

    `SearchListHeaderReusableView` 의 레이아웃을 구성합니다

--- Project/Sources/Domain/Search/ReusableView/SearchListHeaderReusableView.swift
+++ Project/Sources/Domain/Search/ReusableView/SearchListHeaderReusableView.swift
@@ -18,10 +18,21 @@ public final class SearchListHeaderReusableView: UICollectionReusableView {
 
 	// MARK: - UI
 	
+	private let contentView = UIView()
+	
+	private let titleLabel = UILabel().then {
+		$0.font = .boldSystemFont(ofSize: 18)
+		$0.textColor = .black
+	}
+	
 	// MARK: - Initialize
 	
 	public override init(frame: CGRect) {
 		super.init(frame: frame)
+		
+		self.backgroundColor = .white
+		
+		self.addSubview(self.contentView)
 		
 		self.defineFlexContainer()
 	}
@@ -51,9 +62,16 @@ public final class SearchListHeaderReusableView: UICollectionReusableView {
 
 private extension SearchListHeaderReusableView {
 	func defineFlexContainer() {
+		self.contentView.flex
+			.justifyContent(.center)
+			.paddingHorizontal(20)
+			.define {
+				$0.addItem(self.titleLabel).shrink(1)
+			}
 	}
 	
 	func layoutFlexContainer() {
-		self.flex.layout()
+		self.contentView.pin.all()
+		self.contentView.flex.layout()
 	}
 }
