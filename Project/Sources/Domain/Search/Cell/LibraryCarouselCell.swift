9f543105d35fded7f2cce91d8ebcb19dc3bd197d
가대현 <daehyun.ka@musinsa.com>
2024-03-30 07:56:06 +0900

    가로스크롤이 되는 `내 라이브러리 검색결과`를 표시하기 위해 빈 `LibraryCarouselCell`을 추가합니다

---
+++ Project/Sources/Domain/Search/Cell/LibraryCarouselCell.swift
@@ -0,0 +1,63 @@
+//
+//  LibraryCarouselCell.swift
+//  KidsnotePreProject
+//
+//  Created by 가대현 on 3/30/24.
+//
+
+import FlexLayout
+import PinLayout
+import Then
+import UIKit
+
+final class LibraryCarouselCell: UICollectionViewCell {
+	
+	// MARK: - Constants
+	
+	// MARK: - Properties
+
+	// MARK: - UI
+	
+	// MARK: - Initialize
+	
+	override init(frame: CGRect) {
+		super.init(frame: frame)
+		
+		self.defineFlexContainer()
+	}
+	
+	@available(*, unavailable) required init?(coder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+	
+	// MARK: - Life cycle
+	
+	override func layoutSubviews() {
+		super.layoutSubviews()
+		
+		self.layoutFlexContainer()
+	}
+	
+	override func prepareForReuse() {
+		super.prepareForReuse()
+	}
+	
+	deinit {
+		print("LibraryCarouselCell deinit...")
+	}
+
+	// MARK: - Configure
+
+	// MARK: - Logic
+}
+
+// MARK: - Layout
+
+private extension LibraryCarouselCell {
+	func defineFlexContainer() {
+	}
+	
+	func layoutFlexContainer() {
+		self.contentView.flex.layout()
+	}
+}
