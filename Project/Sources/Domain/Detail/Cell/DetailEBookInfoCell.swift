399d05ac1f9a06628a180725ee3b172b60e1119b
가대현 <daehyun.ka@musinsa.com>
2024-03-29 13:17:54 +0900

    상세 화면에 eBook 정보를 표시하기 위해 빈 `DetailEBookInfoCell`을 추가합니다

---
+++ Project/Sources/Domain/Detail/Cell/DetailEBookInfoCell.swift
@@ -0,0 +1,63 @@
+//
+//  DetailEBookInfoCell.swift
+//  KidsnotePreProject
+//
+//  Created by 가대현 on 3/29/24.
+//
+
+import FlexLayout
+import PinLayout
+import Then
+import UIKit
+
+final class DetailEBookInfoCell: UICollectionViewCell {
+	
+	// MARK: - Constants
+	
+	// MARK: - Properties
+
+	// MARK: - UI
+	
+	// MARK: - Initialize
+	
+	override init(frame: CGRect) {
+		super.init(frame: frame)
+		
+		self.defineFlexContainer()
+	}
+	
+	@available(*, unavailable) required init?(coder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+	
+	// MARK: - Life cycle
+	
+	override func layoutSubviews() {
+		super.layoutSubviews()
+		
+		self.layoutFlexContainer()
+	}
+	
+	override func prepareForReuse() {
+		super.prepareForReuse()
+	}
+	
+	deinit {
+		print("DetailEBookInfoCell deinit...")
+	}
+
+	// MARK: - Configure
+
+	// MARK: - Logic
+}
+
+// MARK: - Layout
+
+private extension DetailEBookInfoCell {
+	func defineFlexContainer() {
+	}
+	
+	func layoutFlexContainer() {
+		self.contentView.flex.layout()
+	}
+}
