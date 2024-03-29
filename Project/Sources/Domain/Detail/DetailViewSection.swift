df282c5e03318458766ca6335d4d4748b0920442
가대현 <daehyun.ka@musinsa.com>
2024-03-29 10:56:05 +0900

    `RxDataSources`를 사용하기 위해 `DetailViewSection`에 `volumeInfo` 섹션을 추가합니다

---
+++ Project/Sources/Domain/Detail/DetailViewSection.swift
@@ -0,0 +1,36 @@
+//
+//  DetailViewSection.swift
+//  KidsnotePreProject
+//
+//  Created by 가대현 on 3/29/24.
+//
+
+import RxDataSources
+
+struct DetailViewSection {
+	enum Identity: Hashable {
+		case volumeInfo
+	}
+
+	var identity: Identity
+	var items: [Item]
+}
+
+extension DetailViewSection {
+	enum Item: Hashable {
+		case volumeInfo(VolumeInfo)
+	}
+}
+
+extension DetailViewSection: AnimatableSectionModelType {
+	init(original: DetailViewSection, items: [Item]) {
+		self = original
+		self.items = items
+	}
+}
+
+extension DetailViewSection.Item: IdentifiableType {
+	var identity: Int {
+		self.hashValue
+	}
+}
