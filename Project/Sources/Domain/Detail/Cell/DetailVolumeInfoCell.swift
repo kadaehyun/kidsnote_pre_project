690bf485dd05b05bc7c5e4ea77d7d719e051e7b0
가대현 <daehyun.ka@musinsa.com>
2024-03-29 11:21:53 +0900

    상세 화면에 책 정보를 표시하기 위해 빈 `DetailVolumeInfoCell`을 추가합니다

---
+++ Project/Sources/Domain/Detail/Cell/DetailVolumeInfoCell.swift
@@ -0,0 +1,63 @@
+//
+//  DetailVolumeInfoCell.swift
+//  KidsnotePreProject
+//
+//  Created by 가대현 on 3/29/24.
+//
+
+import FlexLayout
+import PinLayout
+import Then
+import UIKit
+
+final class DetailVolumeInfoCell: UICollectionViewCell {
+	
+	// MARK: - Constants
+	
+	// MARK: - Properties
+
+	// MARK: - UI
+	
+	// MARK: - Initialize
+	
+	override init(frame: CGRect) {
+		super.init(frame: frame)
+		
+		self.defineFlexContainer()
+	}
+	
+	@available(*, unavailable) required init?(coder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+	
+	// MARK: - Life cycle
+	
+	override func layoutSubviews() {
+		super.layoutSubviews()
+		
+		self.layoutFlexContainer()
+	}
+	
+	override func prepareForReuse() {
+		super.prepareForReuse()
+	}
+	
+	deinit {
+		print("DetailVolumeInfoCell deinit...")
+	}
+
+	// MARK: - Configure
+
+	// MARK: - Logic
+}
+
+// MARK: - Layout
+
+private extension DetailVolumeInfoCell {
+	func defineFlexContainer() {
+	}
+	
+	func layoutFlexContainer() {
+		self.contentView.flex.layout()
+	}
+}
