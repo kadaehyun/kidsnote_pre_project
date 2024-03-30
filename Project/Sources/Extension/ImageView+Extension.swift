fdaf843e38756c7fdb2b108ca5e13f86720086ba
가대현 <daehyun.ka@musinsa.com>
2024-03-30 11:15:36 +0900

    이미지 캐싱 기능을 추가합니다

--- Project/Sources/Extension/ImageView+Extension.swift
+++ Project/Sources/Extension/ImageView+Extension.swift
@@ -9,6 +9,12 @@ import UIKit
 
 extension UIImageView {
 	func imageDownload(url: URL) {
+		if let cacheImage = Cache.imageCache.object(forKey: url.absoluteString as NSString) {
+			DispatchQueue.main.async() { [weak self] in
+				self?.image = cacheImage
+			}
+		}
+		
 		var request = URLRequest(url: url)
 		request.httpMethod = "GET"
 
@@ -18,6 +24,8 @@ extension UIImageView {
 			guard let data = data, error == nil, let image = UIImage(data: data) else { return }
 			
 			DispatchQueue.main.async() { [weak self] in
+				Cache.imageCache.setObject(image, forKey: url.absoluteString as NSString)
+				
 				self?.image = image
 			}
 		}.resume()
---
+++ Project/Sources/Service/Cache.swift
@@ -0,0 +1,12 @@
+//
+//  Cache.swift
+//  KidsnotePreProject
+//
+//  Created by 가대현 on 3/30/24.
+//
+
+import UIKit
+
+class Cache {
+	static let imageCache = NSCache<NSString, UIImage>()
+}
